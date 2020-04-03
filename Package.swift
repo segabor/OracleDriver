206fa2a24e9e7719b3dbad9f3e7a02f1ee90c9ae
Gábor Sebestyén <segabor@gmail.com>
2020-04-03 11:18:43 +0200

    Upgrade manifest to version 5.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,14 +14,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/segabor/Codpi", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .systemLibrary(name: "Codpi", providers: [
+            .brew(["odpi"])
+        ]),
+
         .target(
             name: "OracleDriver",
-            dependencies: []),
+            dependencies: ["Codpi"]),
         .testTarget(
             name: "OracleDriverTests",
             dependencies: ["OracleDriver"]),
